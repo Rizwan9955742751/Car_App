51265cee4308beee32f34a9f6415029e286b9ced
Rizwan <rizwan372001@gmail.com>
2025-10-07 14:57:59 +0530

    changes

--- Start.java
+++ Start.java
@@ -4,9 +4,21 @@ public class A{
 	{
 		String name = "Rizwan";
 	}
+<<<<<<< Updated upstream
 	// task n0 101 
 	public  void m3 ()
 	{
 		// logic to insert the data into database
 	}
+=======
+	// taks no 100
+	public  void m2 ()
+	{
+		int a = 20;
+		int b = 30;
+		int c = a+b;
+		
+	}
+	
+>>>>>>> Stashed changes
 }
